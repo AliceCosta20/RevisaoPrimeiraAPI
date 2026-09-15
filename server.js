eef5b664a6c58cd9e4ae8a86d822957fae817417
Alice <alice.c@edu.senai.br>
2026-09-15 09:43:08 -0300

    Update

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+
+//import express from "express";
+//import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// {
+//     "nome": "Pedro",
+//     "curso": "Desenvolvimeno de Sistema"
+// }
+
+let ALUNOS =[
+    {id: 1, nome: "Alice", curso: "Desenvolvimento de Sistemas"},
+    {id: 2, nome: "Brenda", curso: "Cybersegurança"},
+    {id: 3, nome: "Carlos", curso: "Administração"},
+    {id: 4, nome: "Daniel", curso: "Desenvolvimento de Sistemas"},
+];
+
+app.get("/", (req, res)=>{
+    res.json({
+        mensagem: "API alunos funcionando!"
+    });
+});
+const PORTA =3000;
+
+app.listen(PORTA, ()=>{
+    console.log(`Servidor iniciando com sucesso!`); 
+    console.log(`http://localhost:${`${PORTA}`}`); 
+    });
